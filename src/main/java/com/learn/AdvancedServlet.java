6b903850972493af0803f301929d3ad259fbc301
cyriac lukose <cyriaclukosekuruvila@gmail.com>
2023-10-29 13:30:05 +0000

    simple servelet commit

---
+++ src/main/java/com/learn/AdvancedServlet.java
@@ -0,0 +1,24 @@
+package com.learn;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AdvancedServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter writer=resp.getWriter();
+		writer.print("<h1>In advanced servelet<h1>");
+	}
+
+}
